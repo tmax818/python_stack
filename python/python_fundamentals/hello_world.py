c7c507856b3cf62cfedebaf39e8bc27de3573d7c
tmax818 <tylermaxwell661@gmail.com>
2021-11-22 10:35:01 -0800

    add old string stuff

--- python/python_fundamentals/hello_world.py
+++ python/python_fundamentals/hello_world.py
@@ -1,3 +1,4 @@
+from webbrowser import open
 print("Hello world!")
 
 x = "Hello Python"
@@ -39,3 +40,8 @@ fave_food2 = "pizza"
 print("I love to eat {} and {}.".format(
     fave_food1, fave_food2))  # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}.")  # with an f string
+
+
+# old string formatting
+
+open('https://docs.python.org/3/library/stdtypes.html#old-string-formatting')
